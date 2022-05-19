19cd11eeb439f224fc86e598a35070162dc4a5d4
i.kłusek <J6ZkqwVUn>
2022-05-19 14:49:26 +0200

    componentDidMount / Update, MockAPI

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import { users } from 'data/users';
 import UsersListItems from 'components/molecules/UsersListItem/UsersListItem';
-import { Wrapper } from './UserList.style';
-import { StyledList } from './UserList.style';
+import { StyledList, Wrapper } from './UserList.style';
+import { toHaveAccessibleDescription, toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
+
+const mockAPI = (success) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (users) {
+                resolve([...users]);
+            } else {
+                reject({ message: 'Error' });
+            }
+        }, 2000);
+    });
+};
 
 //this. -> you use function/ argument from class
 class UsersList extends React.Component {
     state = {
-        users,
+        users: [],
+        isLoading: true,
     };
+
+    //zachaczanie sie o poszczegolne etapy zycia naszego komponentu
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        mockAPI()
+            .then((data) => {
+                this.setState({ isLoading: false });
+                this.setState({ users: data });
+            })
+            .catch((err) => console.log(err));
+    }
+
+    componentDidUpdate(_, prevState) {
+        if (prevState.isLoading !== this.state.isLoading) {
+            console.log('Loading state has changed');
+        }
+    }
 
     deleteUser = (name) => {
         const filteredUsers = this.state.users.filter((user) => user.name !== name); //create new array with all the elements that pass the statement
@@ -19,6 +49,7 @@ class UsersList extends React.Component {
     render() {
         return (
             <Wrapper>
+                <h1>{this.state.isLoading ? 'Loading...' : 'Students list'}</h1>
                 <StyledList>
                     {this.state.users.map((userData) => (
                         <UsersListItems
