bd04ad9b8be7c6958c099bf74c19a0b77eb40c38
i.kłusek <J6ZkqwVUn>
2022-05-19 15:30:39 +0200

    Inputs, forms, basic of routing -> begining

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -3,38 +3,8 @@ import { users as usersData } from 'data/users';
 import UsersListItems from 'components/molecules/UsersListItem/UsersListItem';
 import { StyledList, Wrapper } from './UserList.style';
 
-const mockAPI = (success) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (usersData) {
-                resolve([...usersData]);
-            } else {
-                reject({ message: 'Error' });
-            }
-        }, 2000);
-    });
-};
-
 const UsersList = () => {
-    const [users, setUsers] = useState([]);
-    const [isLoading, setLoadingState] = useState(true);
-
-    //useEffect , first argument is a function, second is a table of dependencies
-    useEffect(() => {
-        setLoadingState(true);
-        mockAPI() //you get the usersData and then use is as a data in .then(given arg)
-            .then((data) => {
-                setLoadingState(false);
-                setUsers(data);
-            })
-            .catch((err) => console.log(err));
-    }, []); //you need to give empty table even when you don;t use it
-    //When you give empty table / array, the function will be called just once!!
-
-    useEffect(() => {
-        // call function when the state of dependences (value form argument) change
-        console.log('Loading state has changed');
-    }, [isLoading]);
+    const [users, setUsers] = useState(usersData);
 
     const deleteUser = (name) => {
         const filteredUsers = users.filter((user) => user.name !== name); //create new array with all the elements that pass the statement
@@ -43,7 +13,7 @@ const UsersList = () => {
 
     return (
         <Wrapper>
-            <h1>{isLoading ? 'Loading...' : 'Students list'}</h1>
+            <h1>Students list</h1>
             <StyledList>
                 {users.map((userData) => (
                     <UsersListItems deleteUser={deleteUser} key={userData.name} userData={userData} />
