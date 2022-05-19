8cc8c2e21f5e13dc303d99fbc1c312040a1a9493
i.kłusek <J6ZkqwVUn>
2022-05-19 13:03:32 +0200

    class state component | onClick

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -4,16 +4,24 @@ import UsersListItems from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper } from './UserList.style';
 import { StyledList } from './UserList.style';
 
-const UsersList = () => {
-    return (
-        <Wrapper>
-            <StyledList>
-                {users.map((userData) => (
-                    <UsersListItems key={userData.name} userData={userData} />
-                ))}
-            </StyledList>
-        </Wrapper>
-    );
-};
+class UsersList extends React.Component {
+    state = {
+        listTitle: `User's list: `,
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <h1>{this.state.listTitle}</h1>
+                <button onClick={() => this.setState({ listTitle: 'Students list' })}>Change title</button>
+                <StyledList>
+                    {users.map((userData, index) => (
+                        <UsersListItems key={userData.name} userData={userData} />
+                    ))}
+                </StyledList>
+            </Wrapper>
+        );
+    }
+}
 
 export default UsersList;
