e47118d36cb3f7e85f5c9218b2055dafd72590c2
i.kłusek <J6ZkqwVUn>
2022-05-19 13:07:47 +0200

    toogle event | class lifecycle

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -6,14 +6,16 @@ import { StyledList } from './UserList.style';
 
 class UsersList extends React.Component {
     state = {
-        listTitle: `User's list: `,
+        isUsersList: false,
     };
 
     render() {
         return (
             <Wrapper>
-                <h1>{this.state.listTitle}</h1>
-                <button onClick={() => this.setState({ listTitle: 'Students list' })}>Change title</button>
+                <h1>{this.state.isUsersList ? `User's list` : 'Students list'}</h1>
+                <button onClick={() => this.setState({ isUsersList: !this.state.isUsersList })}>
+                    Change title
+                </button>
                 <StyledList>
                     {users.map((userData, index) => (
                         <UsersListItems key={userData.name} userData={userData} />
