589c4955fd4b9ce33499efb698fe3788c72a56b3
i.kłusek <J6ZkqwVUn>
2022-05-25 12:23:02 +0200

    innit commit

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,12 +1,21 @@
 import Navigation from 'components/organisms/Navigation/Navigation';
+import styled from 'styled-components';
 import React from 'react';
 import { Wrapper } from './MainTemplate.styles';
+import { theme } from 'assets/styles/theme';
+
+const SearchBar = styled.div`
+    grid-row: 1/2; //start from row border 1 end on row border 2
+    grid-column: 2/3; //start from column border 2 and on column border 3
+    border: 1px solid ${({ theme }) => theme.colors.darkPurple};
+`;
 
 const MainTemplate = ({ children }) => {
     return (
         <>
             <Wrapper>
                 <Navigation />
+                <SearchBar />
                 {children} {/* take elements that are between the MainTemplate element (in root f.e) */}
             </Wrapper>
         </>
--- src/components/templates/MainTemplate/MainTemplate.styles.js
+++ src/components/templates/MainTemplate/MainTemplate.styles.js
@@ -7,6 +7,7 @@ export const Wrapper = styled.div`
     margin: 0;
     padding: 0;
     display: grid;
-    grid-template-columns: 150px 1fr; //navigation bar will take 150px of the website width
+    grid-template-rows: 90px 1fr;
+    grid-template-columns: 150px 1fr 0.75fr; //navigation bar will take 150px of the website width
     background-color: ${({ theme }) => theme.colors.lightGrey};
 `;
